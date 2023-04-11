5c68ea1d968cbeacff9e7e3690e6a3a5d29d0fee
Vladyslav Yarema <vlad14yarema@gmail.com>
2023-04-11 11:32:30 +0300

    added the first lua file

---
+++ comments_printing.lua
@@ -0,0 +1,13 @@
+-- this is a comment
+
+--[[
+    this is a comment
+    multi
+    line
+    comment
+]]
+
+-- print to the screen
+print("Hello world", "I am Vlad") -- will print hello world
+-- print("Hello Jeff")
+print("Hello world" .. "I am Vlad")
